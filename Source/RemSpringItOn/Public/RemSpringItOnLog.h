df16db2ae894e391e5e53b882de0ac4b0a5b16c6
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2025-09-12 02:21:58 +0800

    Changed: adopt REM_API style

--- Source/RemSpringItOn/Public/RemSpringItOnLog.h
+++ Source/RemSpringItOn/Public/RemSpringItOnLog.h
@@ -3,9 +3,10 @@
 #pragma once
 
 #include "Logging/LogMacros.h"
+#include "Macro/RemMacroUtilities.h"
 
 #define REM_API REMSPRINGITON_API
 
-REM_API DECLARE_LOG_CATEGORY_EXTERN(LogRemSpringItOn, Log, All);
+REM_API DECLARE_LOG_CATEGORY_EXTERN(LogRemSpringItOn, REM_DEFAULT_LOG_VERBOSITY, REM_MAX_LOG_VERBOSITY)
 
 #undef REM_API
